47f577bed8b7c585506622f06bf53dc525c591ea
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2022-03-02 13:26:00 -0800

    Now once hovering we can have different color

--- script.js
+++ script.js
@@ -2,8 +2,9 @@
 const grid = document.querySelector("#grid");
 // size of the grid
 const size = 640;
-// generate 16*16 squares in the grid and listening on button
-// giving user ability to choose dimensions of grid's item
+// colors
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+// generate 16*16 squares in the grid and listening on button and giving user ability to choose dimensions of grid's item
 function reset() {
     const items = document.querySelectorAll('.item');
     items.forEach(item => item.remove())
@@ -11,7 +12,10 @@ function reset() {
     for(let i = 0; i < (dimension * dimension); i++) {
         let item = document.createElement('div');
         item.setAttribute('class', 'item');
-        item.addEventListener('mouseover', () => item.classList.add('changeColor'));
+        item.addEventListener('mouseover', () => {
+            const color = Math.floor(Math.random() * 7); 
+            item.style.backgroundColor = colors[color]
+        });
         item.style.width = size / dimension + "px";
         item.style.height = size / dimension + "px";
         grid.appendChild(item)
@@ -21,7 +25,10 @@ function generateGrid() {
     for(let i = 0; i < 256; i++) {
         let item = document.createElement('div');
         item.setAttribute('class', 'item');
-        item.addEventListener('mouseover', () => item.classList.add('changeColor'))
+        item.addEventListener('mouseover', () => {
+            const color = Math.floor(Math.random() * 7); 
+            item.style.backgroundColor = colors[color]
+        })
         grid.appendChild(item)
     }
     const button = document.querySelector('button');

a17159a800d9f6db2f493b3dda3addd9ffee392d
Soufiane Zouhair <soufianezouhaironline@gmail.com>
2022-03-02 13:09:24 -0800

    Done with reset function and updating the project

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 // select the grid that we want to add the 16 grid to
 const grid = document.querySelector("#grid");
-// generate 16*16 squares in the grid
+// size of the grid
+const size = 640;
+// generate 16*16 squares in the grid and listening on button
+// giving user ability to choose dimensions of grid's item
+function reset() {
+    const items = document.querySelectorAll('.item');
+    items.forEach(item => item.remove())
+    const dimension = parseInt(prompt('Enter specific dimension: '));
+    for(let i = 0; i < (dimension * dimension); i++) {
+        let item = document.createElement('div');
+        item.setAttribute('class', 'item');
+        item.addEventListener('mouseover', () => item.classList.add('changeColor'));
+        item.style.width = size / dimension + "px";
+        item.style.height = size / dimension + "px";
+        grid.appendChild(item)
+    }
+}
 function generateGrid() {
     for(let i = 0; i < 256; i++) {
         let item = document.createElement('div');
@@ -9,12 +25,6 @@ function generateGrid() {
         grid.appendChild(item)
     }
     const button = document.querySelector('button');
-    button.addEventListener('click',() => reset())
+    button.addEventListener('click',() => reset());
 }
 generateGrid()
-
-// change the color of each one when hover on it
-function reset() {
-    const squaresChanged = document.querySelectorAll('.changeColor');
-    squaresChanged.forEach(item => item.classList.remove('changeColor'))
-}
